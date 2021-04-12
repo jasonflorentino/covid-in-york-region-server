ab59ea463b935b261a2e717d887bf62b87fb7aa6
Jason Florentino <jasonmflorentino@gmail.com>
2021-04-12 12:22:53 -0400

    Add helmet and express-rate-limit

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 require('dotenv').config();
 const errorHandler = require('./utils/error_handler');
+const ratelimiter = require('./utils/ratelimiter');
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
 const cors = require('cors');
 const app = express();
 
+// Middlewares
+app.use(morgan('common'));
+app.use (helmet());
 app.use(cors());
-
+app.use(ratelimiter);
 app.use(express.json());
 
-app.use(morgan('short'));
-
+// Routes
 app.use('/api', require('./routes/dailies.controller'));
 
 app.use(errorHandler);
 
+// Server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log('Server listening on port ' + PORT));
